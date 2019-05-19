57a590a017b1f8188195da2179c01d0190120f02
AnatSanzh <anat.sanzh@gmail.com>
2019-05-19 19:41:33 +0300

    Implemented functional of REST server. Will add comments later.

--- server.py
+++ server.py
@@ -1,26 +1,55 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-
-
-"""class RecordResource(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
 """
+This file contains flask server implementation that provides API for records.
+"""
+
+from flask import Flask
+from flask_restful import reqparse, Resource, Api
+
+
+_add_record_parser = reqparse.RequestParser()
+_add_record_parser.add_argument("number", type=str)
+_add_record_parser.add_argument("address", type=str)
+_add_record_parser.add_argument("name", type=str)
+
+_update_record_parser = reqparse.RequestParser()
+_update_record_parser.add_argument("address", type=str)
+_update_record_parser.add_argument("name", type=str)
+
+
+def _get_record_resource_class(storage):
+    class _RecordResource(Resource):
+        def get(self, record_number):
+            return storage.get(record_number)
+
+        def put(self, record_number):
+            return storage.update(record_number, _update_record_parser.parse_args())
+
+        def delete(self, record_number):
+            return storage.remove(record_number)
+
+    return _RecordResource
+
+
+def _get_records_resource_class(storage):
+    class _RecordsResource(Resource):
+        def post(self):
+            return storage.add(_add_record_parser.parse_args())
+
+        def delete(self):
+            return storage.clear()
+
+    return _RecordsResource
 
 
 class Server:
     """Class that allows to interact with phone book over Internet"""
-    def __init__(self):
+    def __init__(self, storage):
         self._underlying_app = Flask(__name__)
         self._underlying_app.debug = False
 
         self._api = Api(self._underlying_app)
-        #self._api.add_resource(TodoSimple, '/<:todo_id>')
-        # todo: implement handlers form requests!!!
+        self._api.add_resource(_get_record_resource_class(storage), '/record/<str:record_number>')
+        self._api.add_resource(_get_records_resource_class(storage), '/records')
 
     def run(self):
         self._underlying_app.run()
