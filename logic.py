81cae15c52f7fcd5c21a24fecb8f710db510bdfc
DanyaPes <danya.peschansky@gmail.com>
2019-05-19 17:49:38 +0300

    logical functions implemented

--- logic.py
+++ logic.py
@@ -1,7 +1,8 @@
-import storage
+from storage import JsonRecordStorage
+import record
 
 
-currentStorage = storage.JsonRecordStorage("data/records_data.json")
+currentStorage = JsonRecordStorage("data/records_data.json")
 
 
 def get_records(offset: int, count: int) -> list:
@@ -12,25 +13,33 @@ def get_records(offset: int, count: int) -> list:
     :param count: Number of records to return
     :return: List with records
     """
-    # todo: implement!!!
-    return []
+    if len(currentStorage.records) > offset + count + 1:
+        return currentStorage.records[offset:offset + count + 1]
+    else:
+        return currentStorage.records[offset:len(currentStorage.records)]
 
 
-def add_record():
-    # todo: implement!!!
+def add_record(new_record: record):
+    currentStorage.records.append(new_record)
     pass
 
 
-def remove_record():
-    # todo: implement!!!
+def remove_record(phone_number: str):
+    for cur_record in currentStorage.records:
+        if cur_record.phone_number == phone_number:
+            currentStorage.records.remove(cur_record)
+            break
     pass
 
 
-def update_record():
-    # todo: implement!!!
+def update_record(phone_number: int, new_record: record):
+    for cur_record in currentStorage.records:
+        if cur_record.phone_number == phone_number:
+            currentStorage.records[currentStorage.records.index(cur_record)] = new_record
+            break
     pass
 
 
 def remove_records_all():
-    # todo: implement!!!
+    currentStorage.records = []
     pass
