35eedcd218cf2ab9cdaee01d8e6dc5407b819faf
DanyaPes <danya.peschansky@gmail.com>
2019-05-19 18:02:27 +0300

    logical functions implemented

--- logic.py
+++ logic.py
@@ -6,13 +6,6 @@ currentStorage = JsonRecordStorage("data/records_data.json")
 
 
 def get_records(offset: int, count: int) -> list:
-    """
-    Returns list of phone book records
-
-    :param offset: From what positions start getting records
-    :param count: Number of records to return
-    :return: List with records
-    """
     if len(currentStorage.records) > offset + count + 1:
         return currentStorage.records[offset:offset + count + 1]
     else:
