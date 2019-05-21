2c453da774a5f873d5ddcf677ba4a3238e5fb938
unknown <manokhin.kpi@gmail.com>
2019-05-21 14:12:59 +0200

    update() is simplified and optimized

--- cui.py
+++ cui.py
@@ -1,5 +1,4 @@
 from storage import JsonRecordStorage
-import os
 
 # import sys
 # from enum import Enum
@@ -17,39 +16,40 @@ class ConsoleInterface:
 #    state_data = {}
 
     def update(self):
-        print("1. Show records")
-        print("2. Add record")
-        print("3. Remove records by phone number")
-        print("4. Erase all records")
+        print("1. Show records \n2. Add record \n3. Remove records by phone number \n4. Erase all records")
         user_input = int(input())
         json_record_obj = JsonRecordStorage("data/records_data.json")
         if user_input == 1:
-            for record in JsonRecordStorage.records:
-                record.write_record()
+            self.show_recs()
         elif user_input == 2:
-            print("Write phone number:")
-            add_phone_number_input = input()
-            print("Write name of the record:")
-            add_name_input = input()
-            print("Write address:")
-            add_address_input = input()
-            JsonRecordStorage.add(json_record_obj, add_phone_number_input, add_name_input, add_address_input)
+            self.add_rec(json_record_obj)
         elif user_input == 3:
-            print("Write phone number:")
-            remove_phone_number_input = input()
-            JsonRecordStorage.remove(json_record_obj, remove_phone_number_input)
+            self.remove_rec(json_record_obj)
         elif user_input == 4:
-            print("All records have been erased")
-            del JsonRecordStorage.records[0 : len(JsonRecordStorage.records)]
-        pass
+            self.remove_all_recs()
+
+    def show_recs(self):
+        for record in JsonRecordStorage.records:
+            record.write_record()
+
+    def add_rec(self, data: JsonRecordStorage):
+        print("Write phone number:")
+        add_phone_number_input = input()
+        print("Write name of the record:")
+        add_name_input = input()
+        print("Write address:")
+        add_address_input = input()
+        JsonRecordStorage.add(data, add_phone_number_input, add_name_input, add_address_input)
+
+    def remove_rec(self, data: JsonRecordStorage):
+        print("Write phone number:")
+        remove_phone_number_input = input()
+        JsonRecordStorage.remove(data, remove_phone_number_input)
+
+    def remove_all_recs(self):
+        print("All records have been erased")
+        del JsonRecordStorage.records[0: len(JsonRecordStorage.records)]
 
     def run(self):
         while True:
             self.update()
-        """Starts CUI"""
-        # todo: implement!!!
-        """
-            starts infinite loop that calls _update function
-            maybe in another thread
-        """
-        pass
